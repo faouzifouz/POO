12976aaa9820c56e8574ff711e8c6b4b64b739eb
Titah Mohamed Faouzi <46715773+faouzifouz@users.noreply.github.com>
2019-04-24 13:29:09 +0200

    update

---
+++ Helper_class1/html.php
@@ -0,0 +1,29 @@
+<?php
+class Html {
+
+    public function css($cssLink){
+
+        echo '<link rel="stylesheet" href="'.$cssLink.'">';
+    }
+    public function meta($name){
+
+        echo '<meta name="'.$name.'">';
+    }
+    public function img($src){
+
+        echo '<img src="'.$src.'">';
+    }
+    public function lien($href){
+
+        echo '<a href="'.$href.'"></a>';
+    }
+}
+
+$html = new Html();
+$html->css("lien css");
+$html->meta("balise meta");
+$html->img("src img");
+$html->lien("lien");
+
+
+?>
